d34a78bd6f8b439f3a7a6fee3bb68fa6c87846f7
ethqnit <ethqnit@gmail.com>
2020-10-11 05:26:17 -0400

    10.11.2020
    Updated

---
+++ Assets/StartAppAds/AdsScript/GDPRManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using StartApp;
+
+public class GDPRManager : MonoBehaviour
+{
+  public int index;
+ public GameObject Manager;
+    void Start()
+    {
+    
+        index = PlayerPrefs.GetInt("index");
+
+        if(index ==2)
+        {
+            Manager.gameObject.SetActive(false);
+
+        }
+        if(index == 3)
+        {
+            Manager.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void Concent()
+    {
+        AdSdk.Instance.SetUserConsent( "pas", true, (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds);
+
+        index = 2;
+        PlayerPrefs.SetInt("index", index);
+    }
+    public void NotConcent()
+    {
+        AdSdk.Instance.SetUserConsent( "pas", false, (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds);
+         index = 3;
+         PlayerPrefs.SetInt("index", index);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(index ==2)
+        {
+            Manager.gameObject.SetActive(false);
+
+        }
+        if(index == 3)
+        {
+            Manager.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/StartAppAds/Plugins/iOS/STAStartAppAdBasic.h
@@ -0,0 +1,14 @@
+//
+//  STAStartAppAdBasic.h
+//  StartApp
+//
+//  Created by StartApp on 3/3/15.
+//  Copyright (c) 2013 StartApp. All rights reserved.
+//  SDK version 4.3.0
+
+#import <Foundation/Foundation.h>
+
+@interface STAStartAppAdBasic : NSObject
+
++(BOOL)showAd;
+@end
